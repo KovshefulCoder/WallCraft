85a375b1b8c4b2bc7bc37c904c864a4df1b24c1e
KovshefulCoder <2323set@gmail.com>
2023-09-15 22:57:57 +0300

    add coil logging

--- app/src/main/java/ru/kovsheful/wallcraft/di/WallCraftApp.kt
+++ app/src/main/java/ru/kovsheful/wallcraft/di/WallCraftApp.kt
@@ -1,7 +1,18 @@
 package ru.kovsheful.wallcraft.di
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.util.DebugLogger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class WallCraftApp: Application()
+class WallCraftApp: Application(), ImageLoaderFactory {
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .crossfade(true)
+            .logger(DebugLogger())
+            .build()
+    }
+}
