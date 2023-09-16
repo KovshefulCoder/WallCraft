7fc7082781fb24b0a3f4ecc18bcc27da8748d95c
KovshefulCoder <2323set@gmail.com>
2023-09-16 20:07:20 +0300

    Create color themes

--- app/src/main/java/ru/kovsheful/wallcraft/ui/theme/Color.kt
+++ app/src/main/java/ru/kovsheful/wallcraft/ui/theme/Color.kt
@@ -10,8 +10,6 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-
-val Background = Color(0xFF17171A)
 val PrimaryColor = Color(0xFF286EF1)
 val TextColor = Color(0xFFFFFFFF)
 val SecondaryText = Color(0xFF90949F)
